8d815f90d86e70a56e15703e1d286b78a5ee778f
Jared Black <jaredblackjcb@gmail.com>
2023-01-30 21:11:03 -0600

    Remove Pegasus examples and generated files

--- assets/javascript/pegasus/pegasus.js
+++
@@ -1,8 +0,0 @@
-import {getApiConfiguration} from "../api";
-import {PegasusApi} from "../api-client";
-
-export { Charts } from './examples/charts';
-
-export function getPegasusApiClient(serverBaseUrl) {
-  return new PegasusApi(getApiConfiguration(serverBaseUrl));
-}

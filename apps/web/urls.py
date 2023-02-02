783d9e1204c0cdd8a50022ffc538456270dc3831
Jared Black <jaredblackjcb@gmail.com>
2023-02-01 23:54:48 -0600

    Update navbars, footer, and navigation

--- apps/web/urls.py
+++ apps/web/urls.py
@@ -6,7 +6,11 @@ from . import views
 app_name = 'web'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('product/', TemplateView.as_view(template_name="web/product.html"), name='product'),
+    path('pricing/', TemplateView.as_view(template_name="web/pricing.html"), name='pricing'),
+    path('about/', TemplateView.as_view(template_name="web/about.html"), name='about'),
     path('terms/', TemplateView.as_view(template_name="web/terms.html"), name='terms'),
+
     # these views are just for testing error pages
     # actual error handling is handled by Django: https://docs.djangoproject.com/en/4.1/ref/views/#error-views
     path('400/', TemplateView.as_view(template_name='400.html'), name='400'),
--- assets/frontend/src/App.js
+++ assets/frontend/src/App.js
@@ -1,7 +1,5 @@
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
-import Footer from "./components/Footer";
 import SideNav from "./components/SideNav";
-import TopNav from "./components/TopNav";
 import HomeScreen from "./screens/HomeScreen";
 
 function App() {
@@ -9,13 +7,11 @@ function App() {
     <div>
       <SideNav />
       <main className="main-content position-relative border-radius-lg ">
-        <TopNav />
         <Router>
           <Routes>
             <Route path="/" element={<HomeScreen />} />
           </Routes>
         </Router>
-        <Footer />
       </main>
     </div>
   );
